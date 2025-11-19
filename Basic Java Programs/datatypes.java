caae7f72d75741dbf6cc9d19122fd81b55dc3f5e
Ali Hasan <ah14hasan@gmail.com>
2025-11-19 14:09:00 +0530

    Add program to demonstrate Java data types
    This program demonstrates the use of different data types in Java by printing an integer, a double, and a string.

---
+++ Basic Java Programs/datatypes.java
@@ -0,0 +1,13 @@
+// PROGRAM TO PRINT VARIABLES OF DIFFERENT DATA TYPES
+import java.util.*;
+import java.io.*;
+class sample1 {
+    public static void main(String[] args) {
+        int x = 45;
+        double d = 3.14;
+        String s = "Hello, Java!";
+        System.out.println("Integer: " + x);
+        System.out.println("Double: " + d);
+        System.out.println("String: " + s);
+    }
+}
