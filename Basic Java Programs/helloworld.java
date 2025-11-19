5de92c59d90f453b8c87131bdbb89e40a8ab3dd9
Ali Hasan <ah14hasan@gmail.com>
2025-11-19 14:07:59 +0530

    Add Hello World program in Java

---
+++ Basic Java Programs/helloworld.java
@@ -0,0 +1,10 @@
+// PROGRAM TO PRINT "HELLO, WORLD!" IN JAVA
+import java.util.*;
+import java.io.*;
+class sample 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+}
