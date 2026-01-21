e357ed26df2d55385946416e8f9b2346b85d2219
Ali Hasan <ah14hasan@gmail.com>
2026-01-21 15:32:02 +0530

    Create 5.Multilevel_Inheritance.java

---
+++ Lab Exam/5.Multilevel_Inheritance.java
@@ -0,0 +1,42 @@
+// PROGRAM TO DEMONSTRATE MULTI-LEVEL INHERITANCE
+import java.util.*;
+import java.io.*;  
+// Base class or parent class
+class GrandParent 
+{
+    void gpMethod() 
+    {
+        System.out.println("This is the grandparent class.");
+    }
+}
+// subclass of GrandParent
+// Child class of GrandParent (intermediate class)
+class Parent extends GrandParent 
+{
+    void parentMethod() 
+    {
+        System.out.println("This is the parent class.");
+    }
+}
+// subclass of Parent
+// Child class of Parent (final derived class)
+class Child extends Parent 
+{
+    void childMethod() 
+    {
+        System.out.println("This is the child class.");
+    }
+}
+
+// Main class to test the multi-level inheritance
+class SampleMultiLevel 
+{
+    public static void main(String[] args) 
+    {
+        // Create object of Child class
+        Child obj = new Child();
+        obj.gpMethod();       // Method from GrandParent
+        obj.parentMethod();   // Method from Parent
+        obj.childMethod();    // Method from Child
+    }
+}
