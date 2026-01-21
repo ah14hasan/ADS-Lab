2c255605404621db57681a53bf6a3b16a36e9364
Ali Hasan <ah14hasan@gmail.com>
2026-01-21 15:39:49 +0530

    Create 9.Interface_Inheritance.java

---
+++ Lab Exam/9.Interface_Inheritance.java
@@ -0,0 +1,68 @@
+// Interface Inheritance
+// Here is a simple Java program that combines:
+//Single/multilevel inheritance using classes
+// Multiple inheritance using interfaces
+import java.util.*;
+import java.io.*; 
+
+// Interfaces (used to simulate multiple inheritance)
+interface CanRun {
+    void run();
+}
+
+interface CanSpeak {
+    void speak();
+}
+
+// Base class
+class Person {
+    void showPerson() {
+        System.out.println("This is a person.");
+    }
+}
+
+// Intermediate subclass (single / multilevel inheritance)
+class Employee extends Person {
+    void showEmployee() {
+        System.out.println("This is an employee.");
+    }
+}
+
+// Hybrid inheritance:
+// Student extends a class (Employee) AND implements two interfaces (CanRun, CanSpeak)
+class Student extends Employee implements CanRun, CanSpeak {
+
+    @Override
+    public void run() {
+        System.out.println("Student can run.");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Student can speak.");
+    }
+
+    void showStudent() {
+        System.out.println("This is a student.");
+    }
+}
+
+// Main class to test hybrid inheritance
+class SampleHybrid {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        // From Person (base class)
+        s.showPerson();
+
+        // From Employee (single / multilevel inheritance)
+        s.showEmployee();
+
+        // From Student
+        s.showStudent();
+
+        // From interfaces (multiple inheritance via interfaces)
+        s.run();
+        s.speak();
+    }
+}
